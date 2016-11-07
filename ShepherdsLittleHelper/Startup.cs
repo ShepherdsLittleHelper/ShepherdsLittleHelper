4a4b16e17c181e6c4e0860d837b9a9635d117310
KurtWeber <weberkrt@msn.com>
2016-11-07 10:34:25 -0500

    Initial commit to build repository

---
+++ ShepherdsLittleHelper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShepherdsLittleHelper.Startup))]
+namespace ShepherdsLittleHelper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
